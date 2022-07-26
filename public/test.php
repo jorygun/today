0b4efd3b89bafe5eb680edcfa50e4151683433a9
techdude <john@digitalmx.com>
2022-07-25 18:29:56 -0700

    fixed

--- public/test.php
+++ public/test.php
@@ -1,7 +1,7 @@
 <?php
 namespace DigitalMx\jotr;
 
-#ini_set('display_errors', 1);
+ini_set('display_errors', 1);
 
 //BEGIN START
 	require $_SERVER['DOCUMENT_ROOT'] . '/init.php';
@@ -45,7 +45,7 @@ if (1) {
 
 $html = 'http://jotr.digitalmx.com/pages/print.html';
 
-`curl -d @{$html} -H "project: OSyxsT8B8RC83MDi" -H "token: 0gaZ43q1NHn9Wj8NdCL7WetJvKj7vIv8bAHQpn8JPqz909nPOzU5eetM8u0v" -X POST "https://api.typeset.sh/ " > test.pdf`;
+`curl -d @{$html} -H "project: OSyxsT8B8RC83MDi" -H "token: 0gaZ43q1NHn9Wj8NdCL7WetJvKj7vIv8bAHQpn8JPqz909nPOzU5eetM8u0v" -X POST https://api.typeset.sh/ > test.pdf`;
 
 // $pdf = Typesetsh\createPdf($html);
 // $pdf->toFile(REPO_PATH . '/public/pages/print.pdf');
